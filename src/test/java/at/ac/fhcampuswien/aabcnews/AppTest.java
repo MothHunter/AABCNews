4aa4524862cb921acf350fa4e5d2d3735b6645c7
Hanaa <Hanaa1311991>
2022-03-03 00:06:15 +0100

    first test

--- src/test/java/at/ac/fhcampuswien/aabcnews/AppTest.java
+++ src/test/java/at/ac/fhcampuswien/aabcnews/AppTest.java
@@ -1,4 +1,15 @@
 package at.ac.fhcampuswien.aabcnews;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class AppTest {
+    @Test
+    public void printWelcomeTest(){
+        String welcomeMassage="*****************************"+System.lineSeparator()+
+                              "  *  welcome to AABCNews  *  "+System.lineSeparator()+
+                              "*****************************";
+    assertEquals(welcomeMassage,App.printwelcome);
+    }
 }
