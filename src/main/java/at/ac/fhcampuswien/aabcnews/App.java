7034564eea4509054f24ec599378c7e01559333a
Florian Bodner <florian.bodner@stud.fh-campuswien.ac.at>
2022-03-03 10:52:01 +0100

    changed to GUI

--- src/main/java/at/ac/fhcampuswien/aabcnews/App.java
+++ src/main/java/at/ac/fhcampuswien/aabcnews/App.java
@@ -7,8 +7,8 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class App /*extends Application*/ {
-    /*
+public class App extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("hello-view.fxml"));
@@ -17,16 +17,9 @@ public class App /*extends Application*/ {
         stage.setScene(scene);
         stage.show();
     }
-     */
+
 
     public static void main(String[] args) {
-        //launch();
-        System.out.println("Hello World!");
-    }
-    public  String printWelcome() {
-        String message = "*****************************"+System.lineSeparator()+
-                "  *  welcome to AABCNews  *  "+System.lineSeparator()+
-                "*****************************";
-        return message;
+        launch();
     }
 }
--- src/test/java/at/ac/fhcampuswien/aabcnews/AppTest.java
+++ src/test/java/at/ac/fhcampuswien/aabcnews/AppTest.java
@@ -5,13 +5,4 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest {
-    @Test
-    public void printWelcomeTest(){
-        App app = new App();
-        String welcomeMassage="*****************************"+System.lineSeparator()+
-                              "  *  welcome to AABCNews  *  "+System.lineSeparator()+
-                              "*****************************";
-        String text = app.printWelcome();
-    assertEquals(welcomeMassage,text);
-    }
 }
