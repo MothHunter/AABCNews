058ddda657ef297015c71672357f8412f01d3c19
Hanaa <Hanaa1311991>
2022-03-03 11:16:37 +0100

    new commit

--- src/main/java/at/ac/fhcampuswien/aabcnews/App.java
+++ src/main/java/at/ac/fhcampuswien/aabcnews/App.java
@@ -11,7 +11,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("menu-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
