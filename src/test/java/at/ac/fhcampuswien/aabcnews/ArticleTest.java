ba8e0447d197de0c7ee502245f2ceb56433b9011
Florian Bodner <florian.bodner@stud.fh-campuswien.ac.at>
2022-03-18 14:17:24 +0100

    wrote tests for the article class

--- src/test/java/at/ac/fhcampuswien/aabcnews/ArticleTest.java
+++ src/test/java/at/ac/fhcampuswien/aabcnews/ArticleTest.java
@@ -1,5 +1,31 @@
 package at.ac.fhcampuswien.aabcnews;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class ArticleTest {
-    //app Test schreiben
+    Article testArticle;
+
+    @BeforeEach
+    public void init() {
+        testArticle = new Article("Rincewind the Wizzard", "The From of running");
+    }
+
+    @Test
+    public void getAuthorTest() {
+        Assertions.assertEquals("Rincewind the Wizzard", testArticle.getAuthor());
+    }
+
+    @Test
+    public void getTitleTest() {
+        Assertions.assertEquals("The From of running", testArticle.getTitle());
+    }
+
+    @Test
+    public void articleToStringTest() {
+        Assertions.assertEquals("Title: The From of running, Author: Rincewind the Wizzard",
+                                testArticle.toString());
+    }
 }
