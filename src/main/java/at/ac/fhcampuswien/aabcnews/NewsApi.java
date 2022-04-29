9c59276d8b604cedd55ac5d3cec916c18eef8440
Lea Christa <leamagdalena24@gmail.com>
2022-04-29 13:00:24 +0200

    this are the enums.

--- src/main/java/at/ac/fhcampuswien/aabcnews/NewsApi.java
+++ src/main/java/at/ac/fhcampuswien/aabcnews/NewsApi.java
@@ -7,6 +7,13 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class NewsApi {
+    public enum Category {business, entertainment, general, health, science, sports, technology}
+    public enum Country {ae, ar, at, au, be, bg, br, ca, ch, cn, co, cu, cz, de, eg, fr, gb, gr,
+        hk, hu, id, ie, il, in, it, jp, kr, lt, lv, ma, mx, my, ng, nl, no, nz, ph, pl, pt, ro, rs,
+        ru, sa, se, sg, si, sk, th, tr, tw, ua, us, ve, za}
+    public enum Language {ar, de, en, es, fr, he, it, nl, no, pt, ru, se, ud, zh}
+    public enum SortBy {relevancy, popularity, publishedAt}
+    //grds immer public enum, Name,{Konstanten}-->quasi extrem reduzierte Klasse
     private static final String root = "https://newsapi.org/v2/";
     private static final String apiKey = "0eb47479ee9b40829604c68ff2adb858";
     private static NewsApi instance;
