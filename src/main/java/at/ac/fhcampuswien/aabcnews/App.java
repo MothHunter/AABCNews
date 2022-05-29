7f76e7684f8c32117f0b226537362ab66b54552e
ramonaAnd <ramo.andrei84@gmail.com>
2022-05-29 14:18:02 +0200

    gui

--- src/main/java/at/ac/fhcampuswien/aabcnews/App.java
+++ src/main/java/at/ac/fhcampuswien/aabcnews/App.java
@@ -12,7 +12,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("menu-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("AABCD News");
         stage.setScene(scene);
         stage.show();
