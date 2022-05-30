f1061b820d2dedc926eefb4ae7fd259ed6812983
Florian Bodner <florian.bodner@stud.fh-campuswien.ac.at>
2022-05-30 23:57:16 +0200

    removed extra ;

--- src/main/java/at/ac/fhcampuswien/aabcnews/NewsApiException.java
+++ src/main/java/at/ac/fhcampuswien/aabcnews/NewsApiException.java
@@ -3,7 +3,7 @@ package at.ac.fhcampuswien.aabcnews;
 public class NewsApiException extends Exception{
     public enum EXCEPTION_CODE { none, badResponse, noConnection, webserviceUnreachable, badList,
         requestConstructionFailed, badDownloadURL, fileCreationFailed
-    };
+    }
     EXCEPTION_CODE exceptionCode = EXCEPTION_CODE.none;
     public NewsApiException(String message) {
         super(message);
